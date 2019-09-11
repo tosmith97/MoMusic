2ae604adc53794bfd0c71c7640bc1e168e47aafd
Tyler Smith <tosmith1997@gmail.com>
2019-09-11 13:25:16 -0700

    Update defs.js

--- backend/app/constants/defs.js
+++ backend/app/constants/defs.js
@@ -1,15 +1,1 @@
 
-exports.SCHOOLS = [
-    'Harvard College',
-    'Stanford University'
-];
-
-exports.SCHOOL_DOMAINS = [
-    'harvard.edu',
-    'stanford.edu'
-];
-
-exports.DOMAIN_MAP = {
-    "Harvard College": "harvard.edu",
-    "Stanford University": "stanford.edu",
-}
